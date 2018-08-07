cda6d3365b4ebc4d2f48d1fc2cf96fc242eaa96c
Ruzzyy <ru.boryan@gmail.com>
2018-08-07 15:53:01 +0300

    четвертый урок. Игры

---
+++ Lesson_4/gameGB/gameCode.js
@@ -0,0 +1,163 @@
+ var event, ok;
+
+do {//Выводим первый вопрос
+    ok = false;
+    event = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры'); //получаем новер варианта ответа
+    if (event == -1) {
+        break;
+    }
+    else {
+        ok = isAnswer(works.a0, event);
+    }
+} while (!ok);
+switch (event) {
+    case 1:
+        do {
+            ok = false;
+            event = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+                ok = isAnswer(works.b0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
+        do {
+            ok = false;
+            event = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+                ok = isAnswer(works.c0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case -1: // Первое действие
+        break;
+    default:
+        alert('Ошибка');
+}
+
+ let newindow = +prompt('Введите номер окна, чтобы вспомнить вопросы');
+switch(newindow){
+    case 1:
+        do {//Выводим первый вопрос
+            ok = false;  //будет равна false если ответ некорректный
+            event = +prompt(works.a00 + '-1 - Выход из игры'); //получаем новер варианта ответа
+            if (event == -1) {
+                break;
+            }
+            else {
+                ok = isAnswer(works.a0, event);
+            }
+        } while (!ok);
+        break;
+    case 2:
+        do {
+            ok = false;
+            event = +prompt(works.b00  + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+                ok = isAnswer(works.b0, event);
+            }
+        } while (!ok);
+        break;
+    case 3:
+        do {
+            ok = false;
+            event = +prompt(works.c00 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+                ok = isAnswer(works.c0, event);
+            }
+        } while (!ok);
+        break;
+    default:
+        alert(' Ошибка ');
+}
+ alert('Спасибо за игру');
+
+//------------------------------------------
+function isAnswer(q, event) {
+    if (isNaN(event) || !isFinite(event)) {
+        alert('Вы ввели недопустимый символ');
+        return false;
+    }
+    else if (event < 1 || event > q) {
+        alert('Ваше число выходит из допустимого диапозона');
+        return false;
+    }
+    return true;
+    
+}
---
+++ Lesson_4/theMillionaireGame/gameCode.js
@@ -0,0 +1,126 @@
+let question = [
+    q1 = {question: 'Белым снегом всё одето -\n' +
+        'Значит, наступает ...\n',
+        a0: 4, //вариантов ответа
+        a: '1 - Лето \n',
+        b: '2 - Осень \n',
+        c: '3 - Зима \n',
+        d: '4 - Весна \n',
+        cash: 200
+    },
+    q2 = {question: 'Лишь только свет дневной потух,\n' +
+        'заухал в темноте ...\n' ,
+        a0: 4,
+        a: '1 - Петух \n',
+        b: '2 - Голубь \n',
+        c: '3 - Филин \n',
+        d: '4 - Василий \n',
+        cash: 300
+    },
+    q3 = {question: 'Под деревом четыре льва,\n' +
+        'Один ушёл, осталось ...\n' ,
+        a0: 4,
+        a: '1 - Два \n',
+        b: '2 - Три \n',
+        c: '3 - Четыре \n',
+        d: '4 - Я сбился со счета \n',
+        cash: 500
+    },
+    q4 = {question: 'С пальмы вниз, на пальму снова\n' +
+        'Ловко прыгает ...\n' ,
+        a0: 4,
+        a: '1 - Корова \n',
+        b: '2 - Лиса \n',
+        c: '3 - Стрекоза  \n',
+        d: '4 - Обезьяна \n',
+        cash: 1000
+    }
+];
+
+let event,
+    ok,
+    i = 0,
+    money = 0;
+
+const isAnswer = (q, event)=> {
+    if (isNaN(event) || !isFinite(event)) {
+        alert('Вы ввели недопустимый символ');
+        return false;
+    }
+    else if (event < 1 || event > q) {
+        alert('Ваше число выходит из допустимого диапозона');
+        return false;
+    }
+    return true;
+};
+
+do{
+    ok = false;
+    event = +prompt(q1.question + q1.a + q1.b + q1.c + q1.d + 'Выигрыш: ' + q1.cash);
+    if(event== -1) {
+        break;
+    }else {
+        ok = isAnswer(question[i].a0, event);
+    }
+    i++;
+}while(!ok);
+
+switch (event){
+    case 3: //первый вопрос. если ответ верный, переходим ко второму вопросу
+        money += q1.cash;
+        do{
+            ok = false;
+            event = +prompt(q2.question + q2.a + q2.b + q2.c + q2.d + 'Выигрыш: ' + q2.cash);
+            if(event== -1) {
+                break;
+            }else {
+                ok = isAnswer(question[i].a0, event);
+            }
+            i++;
+        }while(!ok);
+
+        switch (event){
+            case 3: // если второй ответ верный, переходим к третьему вопросу
+                money += q2.cash;
+                do{
+                    ok = false;
+                    event = +prompt(q3.question + q3.a + q3.b + q3.c + q3.d + 'Выигрыш: ' + q3.cash);
+                    if(event== -1) {
+                        break;
+                    }else {
+                        ok = isAnswer(question[i].a0, event);
+                    }
+                    i++;
+                }while(!ok);
+
+                switch (event) {
+                    case 2: //если 3-й ответ верный - переходим к четвертому вопросу
+                        money += q3.cash;
+                                event = +prompt(q4.question + q4.a + q4.b + q4.c + q4.d + 'Выигрыш: ' + q4.cash);
+                                if (event == -1) {
+                                    break;
+                                }else {
+                                    ok = isAnswer(question[i].a0, event);
+                                }
+
+                        switch (event) {
+                            case 4:
+                                money += q4.cash;
+                                alert('Поздравляем, вы прошли игру.');
+                                break;
+                            default:
+                                break;
+
+                        }
+                    default:
+                        break;
+                }
+            default:
+                break;
+        }
+    default:
+    alert('Ваш выигрыш: ' +money +' руб.');
+    break;
+}
+
+
